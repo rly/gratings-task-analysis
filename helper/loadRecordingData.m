9ccc996d10d11f9c898d5222f7d3ddb87544d3d7
Ryan Ly <rly@princeton.edu>
2018-02-07 23:50:06 -0500

    add unit stability analysis script for mua

---
+++ helper/loadRecordingData.m
@@ -0,0 +1,38 @@
+function [R, D, processedDataDir, blockName] = loadRecordingData(...
+        processedDataRootDir, dataDirRoot, muaDataDirRoot, recordingInfoFileName, sessionInd, muaChannelsToLoad)
+
+%% load recording information
+recordingInfo = readRecordingInfo(recordingInfoFileName);
+recordingInfo = rmfield(recordingInfo, 'muaChannelsToLoad'); % remove b/c we're using passed value
+R = recordingInfo(sessionInd);
+sessionName = R.sessionName;
+pl2FilePath = sprintf('%s/%s/%s', dataDirRoot, sessionName, R.pl2FileName);
+fprintf('Loading %s...\n', pl2FilePath);
+
+%% load recording data
+isLoadSpikes = 0;
+isLoadMua = 1;
+isLoadLfp = 0;
+isLoadSpkc = 0;
+isLoadDirect = 1;
+
+R.spikeChannelsToLoad = NaN;
+R.muaChannelsToLoad = muaChannelsToLoad;
+R.lfpChannelsToLoad = NaN;
+R.spkcChannelsToLoad = NaN;
+
+D = loadPL2(pl2FilePath, muaDataDirRoot, sessionName, R.areaName, isLoadSpikes, isLoadMua, isLoadLfp, isLoadSpkc, isLoadDirect, ...
+        R.spikeChannelPrefix, R.spikeChannelsToLoad, R.muaChannelsToLoad, R.lfpChannelsToLoad, R.spkcChannelsToLoad, R.directChannelsToLoad); 
+
+processedDataDir = sprintf('%s/%s', processedDataRootDir, sessionName);
+if exist(processedDataDir, 'dir') == 0
+    mkdir(processedDataDir);
+end
+fprintf('... done (%0.2f s).\n', toc);
+
+assert(numel(R.blockNames) == numel(D.blockStartTimes));
+blockName = strjoin(R.blockNames(R.gratingsTask3DIndices), '-');
+fprintf('Analyzing block names: %s.\n', blockName);
+
+%% remove spike and event times not during task to save memory
+D = trimSpikeTimesAndEvents(D, R.gratingsTask3DIndices);
---
+++ snapshot_plots/muaStabilityAnalysis.m
@@ -0,0 +1,96 @@
+function muaStabilityAnalysis(processedDataRootDir, dataDirRoot, muaDataDirRoot, recordingInfoFileName, sessionInd, muaChannelsToLoad)
+
+v = 9;
+tic;
+
+fprintf('\n-------------------------------------------------------\n');
+fprintf('MUA Unit Stability Analysis\n');
+fprintf('Session index: %d\n', sessionInd);
+fprintf('MUA Channel to Load: %d\n', muaChannelsToLoad);
+fprintf('Recording info file name: %s\n', recordingInfoFileName);
+fprintf('Processed data root dir: %s\n', processedDataRootDir);
+fprintf('Data root dir: %s\n', dataDirRoot);
+fprintf('MUA data root dir: %s\n', muaDataDirRoot);
+fprintf('Version: %d\n', v);
+fprintf('------------------------\n');
+
+%% input check
+% assert(numel(muaChannelsToLoad) == 1);
+
+%% load recording information
+[R, D] = loadRecordingData(...
+        processedDataRootDir, dataDirRoot, muaDataDirRoot, recordingInfoFileName, sessionInd, muaChannelsToLoad);
+
+%% 
+nUnits = numel(D.allMUAStructs);
+fprintf('Processing %d multi-units...\n', nUnits);
+assert(numel(R.blockNames) == numel(D.blockStartTimes));
+
+%% test for significant spike rate drift over the gratings task blocks
+firstGTaskBlockStartTime = D.blockStartTimes(R.gratingsTask3DIndices(1));
+lastGTaskBlockStopTime = D.blockStopTimes(R.gratingsTask3DIndices(end));
+windowSize = 300; % seconds
+movingWindowStep = floor(windowSize/2);
+
+for j = 1:nUnits
+    spikeStruct = D.allMUAStructs{j};
+    unitName = spikeStruct.name;
+    fprintf('Processing %s (%d/%d = %d%%)... \n', unitName, j, ...
+            nUnits, round(j/nUnits*100));
+    
+    spikeIndices = spikeStruct.ts >= firstGTaskBlockStartTime & ...
+                spikeStruct.ts <= lastGTaskBlockStopTime;
+    spikeTimes = spikeStruct.ts(spikeIndices);
+    
+    % adjust spike times to account for gaps between blocks
+    adjustFactor = zeros(size(spikeStruct.ts));
+    for i = 2:numel(R.gratingsTask3DIndices)
+        spikeIndices2 = spikeStruct.ts >= D.blockStartTimes(R.gratingsTask3DIndices(i)) & ...
+                spikeStruct.ts <= D.blockStopTimes(R.gratingsTask3DIndices(i));
+        % difference between start(i) and stop(i-1)
+        adjustFactor(spikeIndices2) = D.blockStartTimes(R.gratingsTask3DIndices(i)) - D.blockStopTimes(R.gratingsTask3DIndices(i - 1));
+    end
+    spikeTimesAdj = spikeTimes - adjustFactor;    
+    
+    % bin spikes
+    binStartTimes = firstGTaskBlockStartTime:movingWindowStep:lastGTaskBlockStopTime-windowSize;
+    numWindows = numel(binStartTimes);
+    spikesPerBin = nan(numWindows, 1);
+    for k = 1:numWindows
+        spikesPerBin(k) = sum(spikeTimesAdj >= binStartTimes(k) & spikeTimesAdj < binStartTimes(k) + windowSize);
+    end
+    
+    % test for time series stationarity
+    % one issue: this method preferentially labels decreasing time series
+    % as stationary. reversing the order of the data yields different
+    % results.
+    [hIsSpikeRateStationaryFwd,pIsSpikeRateStationaryFwd,stat,cValue,reg] = adftest(spikesPerBin, 'model', 'TS', 'alpha', 0.1);
+    [hIsSpikeRateStationaryRev,pIsSpikeRateStationaryRev,stat,cValue,reg] = adftest(spikesPerBin(end:-1:1), 'model', 'TS', 'alpha', 0.1);
+    
+    % test for monotonic trend with strong alpha
+    % REALLY SHOULD be using the reverse test where H0 = there is a trend
+    % or the above stationarity test, perhaps with a bigger window size
+    [hIsSpikeRateTrend,pIsSpikeRateTrend] = Mann_Kendall(spikesPerBin, 0.005);
+    
+    minMeanSpikeRate = 0.2;
+    if ~hIsSpikeRateTrend && mean(spikesPerBin) / windowSize >= minMeanSpikeRate
+        fprintf('Hooray! Null hypothesis not rejected...\n');
+    end
+    
+    if (hIsSpikeRateStationaryFwd && hIsSpikeRateStationaryRev) && mean(spikesPerBin) / windowSize >= minMeanSpikeRate
+        fprintf('Hooray! Spike rate is stationary.\n');
+    end
+    
+    % alternatively, test whether bins are drawn from a uniform
+    % distribution
+    
+    figure;
+    plot(spikesPerBin);
+%     if hIsSpikeRateStationary
+%         title('Hooray! Spike rate is stationary.');
+%         fprintf('Hooray! Spike rate is stationary.\n');
+%         stop
+%     else
+%         title('Spike rate has a unit root. It is not stationary.');
+%     end
+end
