a22096a80512927d2b72578cb39e64922c1ee62a
Ryan Ly <rly@princeton.edu>
2018-03-11 16:25:23 -0400

    update recording file names and remote access

--- helper/readDataRemotely.m
+++ helper/readDataRemotely.m
@@ -1,8 +1,8 @@
 clear;
 sessionInd = 23;
-processedDataRootDir = 'Y:/rly/gratings-task-analysis/processed_data/';
-dataDirRoot = 'Z:/ryanly/McCartney/originals';
-muaDataDirRoot = 'Y:/rly/simple-mua-detection/processed_data/';
+processedDataRootDir = 'Y:/gratings-task-analysis/processed_data/';
+dataDirRoot = 'Z:/McCartney/originals';
+muaDataDirRoot = 'Y:/simple-mua-detection/processed_data/';
 recordingInfoFileName = 'C:/Users/Ryan/Documents/MATLAB/gratings-task-analysis/recordingInfo2.csv';
 channelsToLoad = 1;
 muaChannelsToLoad = 1;
