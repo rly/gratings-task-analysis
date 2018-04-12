3ae0eacb5db2519449d9b506736f977d5977c2d9
Ryan Ly <rly@princeton.edu>
2018-04-12 01:41:14 -0400

    simplify runner script to read gratings task results json

--- helper/saveGratingsTaskResultsJsonToMatRunner.m
+++ helper/saveGratingsTaskResultsJsonToMatRunner.m
@@ -1,9 +1,6 @@
-function saveGratingsTaskResultsJsonToMatRunner(dataDirRoot, recordingInfoFileName, sessionInd, isZeroDistractors)
-tic;
+function saveGratingsTaskResultsJsonToMatRunner(dataDirRoot, recordingInfoFileName, isZeroDistractors)
 
 fprintf('\n-------------------------------------------------------\n');
-fprintf('Gratings Task MUA Analysis\n');
-fprintf('Session index: %d\n', sessionInd);
 fprintf('Recording info file name: %s\n', recordingInfoFileName);
 fprintf('Data root dir: %s\n', dataDirRoot);
 fprintf('Is zero distractors: %s\n', isZeroDistractors);
@@ -11,25 +8,29 @@ fprintf('------------------------\n');
 
 %% load recording information
 recordingInfo = readRecordingInfo(recordingInfoFileName);
-R = recordingInfo(sessionInd);
-if ~isZeroDistractors
-    R.blockIndices = R.gratingsTask3DIndices;
-    R.gratingsTaskLogIndices = R.gratingsTask3DLogIndices;
-else
-    R.blockIndices = R.gratingsTask0DIndices;
-    R.gratingsTaskLogIndices = R.gratingsTask0DLogIndices;
-    if isnan(R.blockIndices)
-        error('No Block Indices defined for Gratings Task 0D');
+for sessionInd = 1:numel(recordingInfo)
+    tic;
+    R = recordingInfo(sessionInd);
+    if ~isZeroDistractors
+        R.blockIndices = R.gratingsTask3DIndices;
+        R.gratingsTaskLogIndices = R.gratingsTask3DLogIndices;
+    else
+        R.blockIndices = R.gratingsTask0DIndices;
+        R.gratingsTaskLogIndices = R.gratingsTask0DLogIndices;
+        if isnan(R.blockIndices)
+            error('No Block Indices defined for Gratings Task 0D');
+        end
     end
+    blockName = strjoin(R.blockNames(R.blockIndices), '-');
+    sessionName = R.sessionName;
+
+    fprintf('Processing %s...\n', sessionName);
+
+    dataDir = sprintf('%s/%s/', dataDirRoot, sessionName);
+    gratingsTaskLogDir = sprintf('%s/%s', dataDir, sessionName(2:end));
+
+    %% process events and sort them into different conditions
+    saveFileName = sprintf('%s/gratingsTaskResultsJson-%s.mat', gratingsTaskLogDir, blockName);
+    saveGratingsTaskResultsJsonToMat(gratingsTaskLogDir, R.gratingsTaskLogIndices, saveFileName);
+    toc
 end
-blockName = strjoin(R.blockNames(R.blockIndices), '-');
-sessionName = R.sessionName;
-
-fprintf('Processing %s...\n', sessionName);
-
-dataDir = sprintf('%s/%s/', dataDirRoot, sessionName);
-gratingsTaskLogDir = sprintf('%s/%s', dataDir, sessionName(2:end));
-
-%% process events and sort them into different conditions
-saveFileName = sprintf('%s/gratingsTaskResultsJson-%s.mat', gratingsTaskLogDir, blockName);
-readGratingsTaskResultsJson(gratingsTaskLogDir, R.gratingsTaskLogIndices, saveFileName);
