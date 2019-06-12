df680234f479d19b3e9e9cff9b560a1e237f26ba
Ryan Ly <rly@princeton.edu>
2019-06-11 23:35:33 -0700

    clean up createTimeLockedSpdf, add comments

--- gratings_task/createTimeLockedSpdf.m
+++ gratings_task/createTimeLockedSpdf.m
@@ -1,13 +1,40 @@
 function timeLockStruct = createTimeLockedSpdf(spikeTs, eventTimes, eventTimesByLoc, timeLockStruct, kernelSigma)
-% timeLockStruct.window and timeLockStruct.spdfWindowOffset must exist
-nLoc = numel(eventTimesByLoc);
+% Create a struct of spike density functions aligned to event times
+% Parameters:
+% - spikeTs - spike times, in seconds from recording file (0 = start of
+% recording)
+% - eventTimes - times of the events of interest, in seconds from recording
+% file
+% - eventTimesByLoc - same as above, but split by cue location
+% - timeLockStruct - struct containing:
+% --- window and
+% --- spdfWindowOffset which specify the window for cutting the data 
+% (seconds before, seconds after) and the offset window for computing the 
+% spdf
+% - kernelSigma - the sigma for the Gaussian kernel for convolving around
+% spikes
+%
+% Returns:
+%   timeLockStruct with many useful parameters
 
+nLoc = numel(eventTimesByLoc);
 timeLockStruct.kernelSigma = kernelSigma;
+
+% compute time vector at which spdf values will be computed
 timeLockStruct.t = computeTForSpdf(timeLockStruct.window(1), timeLockStruct.spdfWindowOffset, kernelSigma);
 
+% align spike times to event (0 = start of window)
 timeLockStruct.spikeTimes = createnonemptydatamatpt(spikeTs, eventTimes, timeLockStruct.window);
 
+% compute the spike density function and bootstrapped error measure
 [timeLockStruct.spdf,~,timeLockStruct.spdfErr] = fixedPsth(timeLockStruct.spikeTimes, kernelSigma, 2, timeLockStruct.t);
+
+% compute spdf for single trials
+timeLockStruct.spdfByEvent = nan(numel(eventTimes), numel(timeLockStruct.t));
+for i = 1:numel(eventTimes)
+    alignedSpikeTimesThisEvent = createnonemptydatamatpt(spikeTs, eventTimes(i), timeLockStruct.window);
+    timeLockStruct.spdfByEvent(i,:) = fixedPsth(alignedSpikeTimesThisEvent, kernelSigma, 0, timeLockStruct.t); % no error
+end
 
 timeLockStruct.spikeTimesByLoc = cell(nLoc, 1);
 timeLockStruct.spdfByLoc = nan(nLoc, numel(timeLockStruct.t));
@@ -15,6 +42,4 @@ timeLockStruct.spdfErrByLoc = nan(nLoc, numel(timeLockStruct.t));
 for i = 1:nLoc
     timeLockStruct.spikeTimesByLoc{i} = createnonemptydatamatpt(spikeTs, eventTimesByLoc{i}, timeLockStruct.window);
     [timeLockStruct.spdfByLoc(i,:),~,timeLockStruct.spdfErrByLoc(i,:)] = fixedPsth(timeLockStruct.spikeTimesByLoc{i}, kernelSigma, 2, timeLockStruct.t);
-end
-
 end
--- helper/readDataLocally.m
+++ helper/readDataLocally.m
@@ -12,7 +12,10 @@
 % lfpChannels = lfpChannelsToLoad;
 % isZeroDistractors = 0;
 % numRandomizations = 2;
+% isLoadSortedSua = 1;
+% isLoadMua = 0;
 
+% 
 % clear;
 % sessionName = 'M20170311';
 % sessionInd = 8;
@@ -27,10 +30,63 @@
 % lfpChannels = lfpChannelsToLoad;
 % isZeroDistractors = 0;
 % numRandomizations = 2;
+% isLoadSortedSua = 1;
+% isLoadMua = 0;
+
+% clear;
+% sessionName = 'M20170529';
+% sessionInd = 24;
+% processedDataRootDir = 'C:/Users/Ryan/Documents/MATLAB/gratings-task-analysis/processed_data/';
+% dataDirRoot = 'C:\Users\Ryan\Documents\MATLAB\gratings-task-data\';
+% muaDataDirRoot = ['C:\Users\Ryan\Documents\MATLAB\gratings-task-data\' sessionName];
+% suaMuaDataDirRoot = muaDataDirRoot;
+% recordingInfoFileName = 'C:/Users/Ryan/Documents/MATLAB/gratings-task-analysis/recordingInfo2.csv';
+% channelsToLoad = 1:32;
+% muaChannelsToLoad = 1:32;
+% lfpChannelsToLoad = 1:32;
+% lfpChannels = lfpChannelsToLoad;
+% isZeroDistractors = 0;
+% numRandomizations = 2;
+% isLoadSortedSua = 1;
+% isLoadMua = 0;
+
+% clear;
+% sessionName = 'M20170201';
+% sessionInd = 3;
+% processedDataRootDir = 'C:/Users/Ryan/Documents/MATLAB/gratings-task-analysis/processed_data/';
+% dataDirRoot = 'C:\Users\Ryan\Documents\MATLAB\gratings-task-data\';
+% muaDataDirRoot = ['C:\Users\Ryan\Documents\MATLAB\gratings-task-data\' sessionName];
+% suaMuaDataDirRoot = muaDataDirRoot;
+% recordingInfoFileName = 'C:/Users/Ryan/Documents/MATLAB/gratings-task-analysis/recordingInfo2.csv';
+% channelsToLoad = 1:32;
+% muaChannelsToLoad = 1:32;
+% lfpChannelsToLoad = 1:32;
+% lfpChannels = lfpChannelsToLoad;
+% isZeroDistractors = 0;
+% numRandomizations = 2;
+% isLoadSortedSua = 1;
+% isLoadMua = 0;
 
 clear;
-sessionName = 'M20170529';
-sessionInd = 24;
+sessionName = 'M20170127';
+sessionInd = 1;
+processedDataRootDir = 'C:/Users/Ryan/Documents/MATLAB/gratings-task-analysis/processed_data/';
+dataDirRoot = 'C:\Users\Ryan\Documents\MATLAB\gratings-task-data\';
+muaDataDirRoot = ['C:\Users\Ryan\Documents\MATLAB\gratings-task-data\' sessionName];
+suaMuaDataDirRoot = muaDataDirRoot;
+recordingInfoFileName = 'C:/Users/Ryan/Documents/MATLAB/gratings-task-analysis/recordingInfo2.csv';
+channelsToLoad = 1:32;
+muaChannelsToLoad = 1:32;
+lfpChannelsToLoad = 1:32;
+lfpChannels = lfpChannelsToLoad;
+isZeroDistractors = 0;
+numRandomizations = 2;
+isLoadSortedSua = 1;
+isLoadMua = 0;
+
+clear;
+sessionName = 'F20171009';
+sessionInd = 40;
 processedDataRootDir = 'C:/Users/Ryan/Documents/MATLAB/gratings-task-analysis/processed_data/';
 dataDirRoot = 'C:\Users\Ryan\Documents\MATLAB\gratings-task-data\';
 muaDataDirRoot = ['C:\Users\Ryan\Documents\MATLAB\gratings-task-data\' sessionName];
